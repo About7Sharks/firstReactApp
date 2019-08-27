16da87eddbebda7c14f0b079171cceffbf8e1624
Zachary <zacarlin@gmail.com>
2019-08-27 15:26:33 -0400

    firt react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  activateLasers = () => {
+    console.log('this is:', this);
+    alert(`Zac thinks you're cute!`)
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <h2>Welcome to Zac's React App</h2>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Although i'm more comfortable with Vue.js and use it in my day to day tasks, it's important for me to have an understand of entire state of the front-end ecosystem so I can choose the right tool for the job.</p>
+          <br></br>
+        <Button onClick={this.activateLasers} variant="contained" color="primary">
+        Press Me
+      </Button>
         </header>
       </div>
     );
